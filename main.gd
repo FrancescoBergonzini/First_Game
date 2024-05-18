a51afc3b7bc20deeb6d7e74ee7259082f71fb159
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-18 22:36:00 +0200

    input 1.0

--- main.gd
+++ main.gd
@@ -5,4 +5,12 @@ extends Node
 func _ready():
 		$Label.text = "Hello world!"
 		$Label.modulate = Color.CHARTREUSE
+		
+func _input(event):
+	if event.is_action_pressed("my_action"):
+		$Label.modulate = Color.AQUA
+		
+	if event.is_action_released("my_action"):
+		$Label.modulate = Color.BLACK
+		
 
