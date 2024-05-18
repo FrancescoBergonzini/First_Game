5ecab38c14a7f752e0070be5e899b22323308a92
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-18 22:53:27 +0200

    if statement

--- main.gd
+++ main.gd
@@ -1,17 +1,25 @@
 extends Node
 
-var health = 100;
+var health = 1;
 var damage = 1;
 
 func _ready():
-	health = 40
-	health = health * health
+	health = 3
 	print_health()
 	
 func  _input(event):
 	if event.is_action_pressed("my_action"):
 		health -= damage;
-		print_health()
+		
+		if health < 0:
+			health = 0
+			print("we die")
+		elif health < 1:
+			print("im about to die")
+		else: 
+			print_health()
+		
+
 		
 #helpers
 func print_health():
