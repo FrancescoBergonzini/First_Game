86438976da0924d522fe09e6ea6fe58dbc7db03b
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-19 12:35:43 +0200

    comment

--- main.gd
+++ main.gd
@@ -1,29 +1,10 @@
 extends Node
 
-var health = 1;
-var damage = 1;
 
 func _ready():
-	health = 3
-	print_health()
-	
-func  _input(event):
-	if event.is_action_pressed("my_action"):
-		health -= damage;
-		
-		if health < 0:
-			health = 0
-			print("we die")
-		elif health < 1:
-			print("im about to die")
-		else: 
-			print_health()
-		
-
-		
-#helpers
-func print_health():
-	print(health)
+	#Comment line
+	#Use Control + K 
+	print("Hello, world!") #Comment
 
 
 
