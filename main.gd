88c320dfce79fce14868b55cb2fbf9aa356d4d5b
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-21 08:59:31 +0200

    enum

--- main.gd
+++ main.gd
@@ -1,22 +1,17 @@
 extends Node
 
+enum OBJ_State
+{
+	ALLY,
+	NEUTRAL,
+	ENEMY
+}
+
+@export var current_state : OBJ_State
+
 func _ready():
-	var players = {
-		"Luca": 1,
-		"Marco": 2,
-		"Francesco": 4, 
-		}
-		
-	players["Marco"] = 10
-	players["Montone"] = 12
-	
-	for name in players:
-		print(name + ": " + str(players[name]))
+	print(current_state)
+	pass
 
-	var advance_players = {
-		"LUCA": {"Level": 1, "Health": 10, "Power": 5},
-		"MARCO": {"Level": 1, "Health": 20, "Power": 10},
-	}
-	
-	print(advance_players["MARCO"]["Power"])
-
+func change_state(OBJ_State):
+	current_state = OBJ_State
