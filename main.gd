6996ca8b165c05660afe9759188afde492a3600c
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-23 08:57:44 +0200

    modifyng node 2.0

--- main.gd
+++ main.gd
@@ -1,20 +1,21 @@
 extends Node
 
-enum Stagioni { INVERNO, AUTUNNO, ESTATE, PRIMAVERA, NONE}
+#onready inizialize the variable in the _ready fuction
+# $ stand for get_node("Player/Weapon")
+# use realative path
+@onready var weapon = $Player/Weapon
 
-@export var stagione = Stagioni.INVERNO
+#
+@export var my_node = Node
+
+@export var my_2DSprite = Sprite2D
+
 
 func _ready():
-	match stagione:
-		Stagioni.INVERNO:
-			print("Inverno")
-		Stagioni.AUTUNNO:
-			print("Autunno")
-		Stagioni.PRIMAVERA:
-			print("Primavera")
-		Stagioni.ESTATE:
-			print("Estate")
-		_:
-			print("default")
-			
+	
+	print(weapon.get_path())
+	
+	if my_node is Node2D:
+		#this use inheritance...
+		print("Is a 2D node")
 	pass
