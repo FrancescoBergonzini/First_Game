b24491dbd1c894d94eb5f614243f844b8e26414a
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-26 11:25:29 +0200

    signal

--- main.gd
+++ main.gd
@@ -1,21 +1,33 @@
 extends Node
 
-#onready inizialize the variable in the _ready fuction
-# $ stand for get_node("Player/Weapon")
-# use realative path
-@onready var weapon = $Player/Weapon
+#signal con str come parametro
+signal leveled_up(message)
 
-#
-@export var my_node = Node
-
-@export var my_2DSprite = Sprite2D
-
+var level : int = 0
+var xp = 0
+@export var xp_increment = 0
 
 func _ready():
+	leveled_up.connect(_on_leveled_up) # +=
+	#leveled_up.disconnect(_on_leveled_up) # -=
 	
-	print(weapon.get_path())
+
+func _on_timer_timeout():
+	xp += xp_increment
+	print("timer finito")
 	
-	if my_node is Node2D:
-		#this use inheritance...
-		print("Is a 2D node")
-	pass
+	leveled_up.emit("String passata come parametro") # Invoke
+	
+	if xp > 20:
+		level_up()
+		pass # Replace with function body.
+	
+func level_up():
+	xp = 0
+	level += 1
+	print("new level:" + str(level))
+
+
+func _on_leveled_up(message):
+	print(message)
+	pass # Replace with function body.
