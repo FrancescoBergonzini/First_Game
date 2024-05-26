e71749980d75c8a2a8b8bc8c75c4c54837816001
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-26 11:52:34 +0200

    get set

--- main.gd
+++ main.gd
@@ -1,33 +1,46 @@
 extends Node
 
-#signal con str come parametro
-signal leveled_up(message)
+#region Set
+signal healthChange(new_health)
+signal gameStart()
 
-var level : int = 0
-var xp = 0
-@export var xp_increment = 0
-
+var health := 100:
+	set(value):
+		if value < 0:
+			print("Health andrebbe sotto zero, clampata...")
+		
+		health = clamp(value, 0, 100)
+		healthChange.emit(health)
+	
 func _ready():
-	leveled_up.connect(_on_leveled_up) # +=
-	#leveled_up.disconnect(_on_leveled_up) # -=
-	
-
-func _on_timer_timeout():
-	xp += xp_increment
-	print("timer finito")
-	
-	leveled_up.emit("String passata come parametro") # Invoke
-	
-	if xp > 20:
-		level_up()
-		pass # Replace with function body.
-	
-func level_up():
-	xp = 0
-	level += 1
-	print("new level:" + str(level))
+	gameStart.connect(OnGameStart)
+	gameStart.emit()
+	pass
 
 
-func _on_leveled_up(message):
-	print(message)
+func _on_health_change(new_health):
+	print("health: " + str(new_health))
 	pass # Replace with function body.
+#endregion
+
+var chance := 0.2
+var chance_pct: int:
+	get:
+		return chance * 100
+	set(value):
+		chance = float(value) / 100.0
+		
+
+func OnGameStart():
+	#set
+	health = -200
+	print("Game Started")
+	
+	#get
+	print(chance_pct)
+	chance = 0.9
+	chance_pct = 50
+	print(chance_pct)
+
+		
+
