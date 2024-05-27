7933176ea9b1f67ca7e0bc631893bb7ebdd0ac4c
FrancescoBergonzini <fbergonzinigames@gmail.com>
2024-05-27 08:11:19 +0200

    inheritance

--- Character.gd
+++ Character.gd
@@ -2,13 +2,7 @@ class_name  Chracter
 
 extends Node
 
-@export var profession : String
-@export var health: int
 
 func _ready():
 		pass
 
-
-func  die():
-	health = 0
-	print(profession + " has die")
--- main.gd
+++ main.gd
@@ -1,15 +1,7 @@
 extends Node
 
-var chest := Equipment.new()
-var legs := Equipment.new()
 
 func _ready():
-	chest.armor = 20
-	legs.wight = 10
-	print(chest.armor)
-
-class Equipment:
-	var armor := 10
-	var wight := 5
+	pass
 		
 
